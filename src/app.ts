9803038c50f7377c3d1c18798fbde5f5ab314547
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-06-07 08:41:46 +0200

    type guards

--- src/app.ts
+++ src/app.ts
@@ -23,3 +23,55 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+//          Type Guards
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation(empl1);
+printEmployeeInformation({ name: "Andrea", startDate: new Date() });
+
+// instanceof - works with classes
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading cargo..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const vehicle1 = new Car();
+const vehicle2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(vehicle1);
+useVehicle(vehicle2);
